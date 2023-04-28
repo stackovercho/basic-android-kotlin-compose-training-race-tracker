a4dc50174d6e1c99b56589c9f71c6ad54eeb6db6
Hyun <hyun.cho4@gmail.com>
2023-04-28 10:57:37 -0400

    both players start moving in their own separate threads and can run concurrently.  however, they're both within the same coroutineScope, so after they both finish executing, their coroutineScope ends.  their coroutineScope was suspending the outer coroutineScope from continuing, so once they finish executing, the outer coroutine scope can continue its execution.  this allows the raceInProgress to change to finished.

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -16,9 +16,11 @@
 
 package com.example.racetracker.ui
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
 
 /**
@@ -43,9 +45,14 @@ class RaceParticipant(
         private set
 
     suspend fun run() {
-        while (currentProgress < maxProgress) {
-            delay(progressDelayMillis)
-            currentProgress += progressIncrement
+        try {
+            while (currentProgress < maxProgress) {
+                delay(progressDelayMillis)
+                currentProgress += progressIncrement
+            }
+        } catch (e: CancellationException) {
+            Log.e("RaceParticipant", "$name: ${e.message}")
+            throw e // Always re-throw CancellationException
         }
     }
 
